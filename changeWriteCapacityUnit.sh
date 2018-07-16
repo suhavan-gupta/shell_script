d15c063bfd9663ff35b869306f87e7cb89bb301e
suhavan_gupta <suhavan@particle41.com>
2018-07-16 19:13:31 +0530

    shell script to change the number of WRITECAPACITYUNITS

---
+++ changeWriteCapacityUnit.sh
@@ -0,0 +1,28 @@
+# ./changeWriteCapacityUnit.sh table_name no_of_write_capacity_units
+# this script is used to change the number of WriteCapacityUnits in dynamodb. It returns with 0 in case of failure
+aws dynamodb update-table --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=$2 --table-name $1 || return 1
+
+while [ "$(aws dynamodb describe-table --table-name $1 --query 'Table.[TableStatus]' --output text)" = "UPDATING" ]
+do 
+	echo "Status : UPDATING"
+	sleep 5
+done
+
+TableStatus="$(aws dynamodb describe-table --table-name $1 --query 'Table.[TableStatus]' --output text)"
+
+WriteCapacityUnits="$(aws dynamodb describe-table --table-name $1 --query 'Table.ProvisionedThroughput.WriteCapacityUnits' --output text)"
+
+if [ "$TableStatus" = "ACTIVE" ]
+    then
+if [ "$WriteCapacityUnits" = "$2" ]
+        then
+            echo "WriteCapacityUnit Successfully changed"
+            return 0
+        else
+	        echo "WriteCapacityUnits are not updated!!!"
+            return 1
+        fi
+else
+    echo "Something went wrong. TableStatus not active"
+    return 1
+fi
